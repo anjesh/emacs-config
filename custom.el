44be17216efde7791415bc00dd7de652e4ac6ee2
Anjesh <anjeshtuladhar@gmail.com>
2025-12-14 16:13:55 +0545

    chore: Update custom.el with package-vc-selected-packages

--- custom.el
+++ custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((gemini-cli :url "https://github.com/linchen2chris/gemini-cli.el"))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
