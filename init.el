e45e7f3d70f1ecf6b7a953d72a8dc0217b2f05e5
Anjesh <anjeshtuladhar@gmail.com>
2026-06-18 17:56:31 +0545

    Extract ibuffer config into module

--- init.el
+++ init.el
@@ -55,22 +55,7 @@
   :ensure t)
 
 (require 'my-treemacs)
-
-(use-package ibuffer
-  :ensure t
-  :init
-  (setq ibuffer-expert t)
-  (setq ibuffer-show-empty-buffers nil)
-  :config
-  (setq ibuffer-formats
-        '((mark
-           (name 18 18 :left :elide)
-           (size 9 -1 :right)
-           (mode 14 14 :left :elide)
-           (read-only 4 4 :left)
-           (modified 3 3 :left)
-           (filename 40 0 :left :elide))))
-  :bind (:map global-map ("C-x C-b" . ibuffer)))
+(require 'my-ibuffer)
 
 (use-package projectile
   :ensure t
